6ad7f9020bc21c1a7516dc250f4ca3e60c7dc3c6
kebele <ykemalbeler@gmail.com>
2020-08-24 09:11:37 +0300

    ekleme

--- kelimelik-vue/src/App.vue
+++ kelimelik-vue/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="card mt-4">
+      <div class="card-header">soru</div>
+      <div class="card-body">
+        <div class="harf shadow">
+          <span>X</span>
+        </div>
+      </div>
+      <div class="card-footer">işlemler</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .harf{
+    width: 100px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px;
+  }
 </style>
